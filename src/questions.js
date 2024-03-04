8150a87ccd3e120b4ef53d075f12b2d7bc376012
Lcole.engr <lcole.engr@gmail.com>
2024-03-04 09:03:25 -0500

    created question bank to be used by main app

---
+++ src/questions.js
@@ -0,0 +1,102 @@
+export default [
+    {
+      id: 'q1',
+      text: 'Which country has won the most FIFA World Cup titles?',
+      answers: [
+        'A) Brazil',
+        'B) Germany',
+        'C) Argentina',
+        'D) Italy',
+      ],
+    },
+    {
+      id: 'q2',
+      text: 'Who is the all-time leading scorer in NBA history?',
+      answers: [
+        'A) LeBron James',
+        'B) Kobe Bryant',
+        'C) Michael Jordan',
+        'D) Kareem Abdul-Jabbar',
+      ],
+    },
+    {
+      id: 'q3',
+      text: 'In which year did Serena Williams win her first Grand Slam singles title?',
+      answers: [
+        'A) 1999',
+        'B) 2001',
+        'C) 2003',
+        'D) 2005',
+      ],
+    },
+    {
+      id: 'q4',
+      text: 'What is the maximum number of clubs a golfer is allowed to carry in their bag during a round of golf?',
+      answers: [
+        'A) 12',
+        'B) 14',
+        'C) 16',
+        'D) 18',
+      ],
+    },
+    {
+      id: 'q5',
+      text: 'Which city hosted the 2016 Summer Olympics?',
+      answers: [
+        'A) Tokyo',
+        'B) Rio de Janeiro',
+        'C) London',
+        'D) Beijing',
+      ],
+    },
+    {
+      id: 'q6',
+      text: 'Who holds the record for the fastest 100m sprint in history?',
+      answers: [
+        'A) Usain Bolt',
+        'B) Carl Lewis',
+        'C) Tyson Gay',
+        'D) Justin Gatlin',
+      ],
+    },
+    {
+      id: 'q7',
+      text: 'What sport is played in the Super Bowl?',
+      answers: [
+        'A) Basketball',
+        'B) Baseball',
+        'C) American Football',
+        'D) Soccer',
+      ],
+    },
+    {
+      id: 'q8',
+      text: 'Which country won the most gold medals at the 2012 London Olympics?',
+      answers: [
+        'A) United States',
+        'B) China',
+        'C) Great Britain',
+        'D) Russia',
+      ],
+    },
+    {
+      id: 'q9',
+      text: 'Which tennis player is known as "The King of Clay"?',
+      answers: [
+        'A) Roger Federer',
+        'B) Rafael Nadal',
+        'C) Novak Djokovic',
+        'D) Andy Murray',
+      ],
+    },
+    {
+      id: 'q10',
+      text: 'Who won the 2019 FIFA Women\'s World Cup?',
+      answers: [
+        'A) United States',
+        'B) Germany',
+        'C) Sweden',
+        'D) Netherlands',
+      ],
+    },
+  ];
