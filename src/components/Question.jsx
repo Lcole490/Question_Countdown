7329ff46cd77d1dc82c85b295055f312e62e511d
Lcole.engr <lcole.engr@gmail.com>
2024-03-05 11:25:30 -0500

    fixed bug in order to prevent user from clicking more than one answer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,45 @@
 import QuestionTimer from "./QuestionTimer";
 import Answers from "./Answers";
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
 
 
-export default function Question({questionText, answers, onSelectAnswer, selectedAnswer, answerState, onSkippedAnswer}){
+export default function Question({questionIndex,  onSelectAnswer, onSkippedAnswer}){
+
+   const [answer, setAnswer] = useState({
+        selectedAnswer: '',
+        isCorrect: null
+    });
+
+    function handleSelectAnswer(answer){
+        setAnswer({
+            selectedAnswer: answer,
+            isCorrect: null
+        })
+
+        setTimeout(() =>{
+            setAnswer({
+                selectedAnswer: answer,
+                isCorrect: QUESTIONS[questionIndex].answers[0] === answer
+            })
+
+
+            setTimeout(() =>{
+                onSelectAnswer(answer);
+            }, 2000)
+        }, 1000);
+    }
+
+
+
+    let answerState = '';
+
+    if(answer.selectedAnswer && answer.isCorrect !== null){
+        answerState = answer.isCorrect ? 'correct' : 'wrong';
+    }
+    else if( answer.selectedAnswer){
+        answerState = 'answered';
+    }
 
     return(
         
@@ -10,14 +47,14 @@ export default function Question({questionText, answers, onSelectAnswer, selecte
 
         <QuestionTimer  timeout={10000} onTimeout={onSkippedAnswer} />
 
-         <h2 > {questionText} </h2>
+         <h2 > {QUESTIONS[questionIndex].text} </h2>
 
          <Answers 
            
-            answers={answers} 
-            selectedAnswer={selectedAnswer} 
+            answers={QUESTIONS[questionIndex].answers} 
+            selectedAnswer={answer.selectedAnswer} 
             answerState={answerState}
-            onSelect={onSelectAnswer}/>
+            onSelect={handleSelectAnswer}/>
         
 
     </div>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,11 +6,9 @@ import Question from './Question.jsx';
 export default function Quiz(){
 
 
-    const [answerState, setAnswerState] = useState('');
-
     const [userAnswers, setUserAnswers] = useState([]);
 
-    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
+    const activeQuestionIndex =  userAnswers.length;
 
 
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
@@ -20,7 +18,7 @@ export default function Quiz(){
 
    const handleSelectedAnswer = useCallback(function handleSelectedAnswer(selectedAnswer){
 
-        setAnswerState('answered');
+      
         setUserAnswers((prevUserAnswers)=>{
            return(
             [...prevUserAnswers, selectedAnswer]
@@ -28,19 +26,8 @@ export default function Quiz(){
 
         });
 
-        setTimeout(()=>{
-            if (selectedAnswer===QUESTIONS[activeQuestionIndex].answers[0]){
-                setAnswerState('correct');
-            }
-            else{
-                setAnswerState('wrong');
-            }
-
-            setTimeout(() =>{
-                setAnswerState('');
-            }, 2000);
-        }, 1000)
-    }, [activeQuestionIndex]);
+       
+    }, []);
 
     const handleSkippedAnswer = useCallback(() =>{
 
@@ -66,10 +53,7 @@ export default function Quiz(){
 
         <Question 
         key={activeQuestionIndex}
-        questionText={QUESTIONS[activeQuestionIndex].text} 
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length -1]}
+        questionIndex={activeQuestionIndex}
         onSelectAnswer={handleSelectedAnswer}
         onSkippedAnswer={handleSkippedAnswer} />
 
